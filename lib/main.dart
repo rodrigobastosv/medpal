8019cbc96755b64004a98e580c37c1c005c0ac58
Rodrigo Vasconcelos <rodrigo.vasconcelos@wendys.com>
2025-12-21 14:16:20 -0300

    Revert add for Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:medpal/core/app/mp_app.dart';
 import 'package:medpal/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  usePathUrlStrategy();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final sharedPreferences = await SharedPreferences.getInstance();
   runApp(MPApp(sharedPreferences: sharedPreferences));
