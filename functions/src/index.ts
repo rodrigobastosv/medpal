b261daa181f3265db9d0f0c19c3c9b6b745573e7
Rodrigo Vasconcelos <rodrigo.vasconcelos@wendys.com>
2026-01-02 08:59:51 -0300

    Create initial firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,77 @@
+import * as admin from "firebase-admin";
+import { onDocumentCreated, onDocumentDeleted } from "firebase-functions/v2/firestore";
+
+admin.initializeApp();
+
+export const onAppointmentCreatd = onDocumentCreated(
+    "users/{userId}/appointments/{appointmentId}",
+    async (event) => {
+        const { userId, appointmentId } = event.params;
+
+        const document = event.data;
+        if (!document) {
+            return;
+        }
+        const { patientId, type, appointmentDate } = document.data();
+
+        await admin
+            .firestore()
+            .collection("calendar")
+            .doc(userId)
+            .collection("appointments")
+            .doc(appointmentId)
+            .set(
+                {
+                    userId,
+                    appointmentId,
+                    patientId,
+                    type,
+                    appointmentDate,
+                },
+                { merge: true }
+            );
+    }
+);
+
+export const onAppointmentDeleted = onDocumentDeleted(
+    "users/{userId}/appointments/{appointmentId}",
+    async (event) => {
+        const { userId, appointmentId } = event.params;
+
+        admin
+            .firestore()
+            .collection("calendar")
+            .doc(userId)
+            .collection("appointments")
+            .doc(appointmentId)
+            .delete();
+    }
+);
+
+export const onPatientDeleted = onDocumentDeleted(
+    "users/{userId}/patients/{patientId}",
+    async (event) => {
+        const { userId, patientId } = event.params;
+
+        const db = admin.firestore();
+        const querySnapshot = await db
+            .collection("users")
+            .doc(userId)
+            .collection("appointments")
+            .where("patientId", "==", patientId)
+            .get();
+
+        if (querySnapshot.empty) {
+            return;
+        }
+
+        const batch = db.batch();
+
+        querySnapshot.docs.forEach((doc) => {
+            batch.delete(doc.ref);
+        });
+
+        await batch.commit();
+
+    }
+);
